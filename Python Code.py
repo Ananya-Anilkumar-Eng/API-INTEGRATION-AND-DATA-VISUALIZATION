fbe25abcc6e969081d0da42417b5e58cd97f846e
Ananya Anilkumar <ananyaanilkumar58@gmail.com>
2025-06-02 16:58:52 +0530

    Create Python Code.py
    Source code of the project.

---
+++ Python Code.py
@@ -0,0 +1,55 @@
+import requests
+import pandas as pd
+import warnings as wg
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Suppressing warning messages
+wg.simplefilter(action="ignore",category="Future Warning")
+
+#Giving a request to access the required public api
+url = "https://disease.sh/v3/covid-19/states"
+response = requests.get(url)
+
+#Converting to json format
+data = response.json()
+
+#Converting to datframe using pandas
+df=pd.DataFrame(data)
+
+#Printing the column names
+print(df.columns)
+
+#Histogram-Bar graph
+sns.barplot(data=df[:20],x='deaths',y='state',color='red')
+plt.title('Distribution of COVID-19 Deaths Across first 20 States')
+plt.xlabel('Number of Deaths')
+plt.ylabel('States')
+sns.histplot(data=df[:15], x='population',color='blue')
+plt.title('Distribution of Population Across first 15 States')
+plt.xlabel('Population')
+plt.ylabel('Count')
+
+#Normal curves
+sns.set(font_scale=1.5)
+sns.catplot(x='population',y='state',data=df[:10],kind='point',capsize=0.4,errorbar=None,aspect=2,color='red')
+plt.title('Distribution of Population Across first 10 States')
+plt.xlabel('Population')
+plt.ylabel('States')
+sns.set(font_scale=1)
+sns.relplot(x='population',y='state',data=df[:10],kind='line',errorbar=None,aspect=2,markers=True,dashes=False,color='red')
+plt.title('Distribution of Population Across first 10 States')
+plt.xlabel('Population')
+plt.ylabel('States')
+
+#Scatterplots, Density and Contour Plots
+sns.set_style('white')
+sns.kdeplot(data=df[:10],x='population',y='deaths')
+plt.title('Distribution of COVID-19 death cases Across first 10 States')
+plt.xlabel('Population')
+plt.ylabel('Deaths')
+sns.set_style('white')
+sns.scatterplot(data=df[:10],x='population',y='deaths')
+plt.title('Distribution of COVID-19 death cases Across first 10 States')
+plt.xlabel('Population')
+plt.ylabel('Deaths')
