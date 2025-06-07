2e8520ba6212f1939f42a5b2b4f2094f236dc827
Ananya Anilkumar <ananyaanilkumar58@gmail.com>
2025-06-07 20:44:39 +0530

    Update Python Code

--- Python Code.py
+++ Python Code.py
@@ -3,10 +3,9 @@ import pandas as pd
 import warnings as wg
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
-#Suppressing warning messages
-wg.simplefilter(action="ignore",category="Future Warning")
-
+wg.simplefilter(action="ignore",category=FutureWarning)
 #Giving a request to access the required public api
 url = "https://disease.sh/v3/covid-19/states"
 response = requests.get(url)
@@ -17,18 +16,17 @@ data = response.json()
 #Converting to datframe using pandas
 df=pd.DataFrame(data)
 
-#Printing the column names
-print(df.columns)
-
 #Histogram-Bar graph
 sns.barplot(data=df[:20],x='deaths',y='state',color='red')
 plt.title('Distribution of COVID-19 Deaths Across first 20 States')
 plt.xlabel('Number of Deaths')
 plt.ylabel('States')
+plt.show()
 sns.histplot(data=df[:15], x='population',color='blue')
 plt.title('Distribution of Population Across first 15 States')
 plt.xlabel('Population')
 plt.ylabel('Count')
+plt.show()
 
 #Normal curves
 sns.set(font_scale=1.5)
@@ -36,11 +34,13 @@ sns.catplot(x='population',y='state',data=df[:10],kind='point',capsize=0.4,error
 plt.title('Distribution of Population Across first 10 States')
 plt.xlabel('Population')
 plt.ylabel('States')
+plt.show()
 sns.set(font_scale=1)
 sns.relplot(x='population',y='state',data=df[:10],kind='line',errorbar=None,aspect=2,markers=True,dashes=False,color='red')
 plt.title('Distribution of Population Across first 10 States')
 plt.xlabel('Population')
 plt.ylabel('States')
+plt.show()
 
 #Scatterplots, Density and Contour Plots
 sns.set_style('white')
@@ -48,8 +48,14 @@ sns.kdeplot(data=df[:10],x='population',y='deaths')
 plt.title('Distribution of COVID-19 death cases Across first 10 States')
 plt.xlabel('Population')
 plt.ylabel('Deaths')
+plt.show()
 sns.set_style('white')
 sns.scatterplot(data=df[:10],x='population',y='deaths')
 plt.title('Distribution of COVID-19 death cases Across first 10 States')
 plt.xlabel('Population')
 plt.ylabel('Deaths')
+plt.show()
+
+#3D Plot
+fig=px.scatter_3d(df[:5],x='population',y='state',z='deaths')
+fig.show()
